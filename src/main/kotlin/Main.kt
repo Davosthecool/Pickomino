cc090fb0c4615d49f085ce58207611406f7a2143
E221936K <nils.moreau--thomas@etu.univ-nantes.fr>
2023-06-15 13:15:39 +0200

    fix

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,21 +24,23 @@ class Pickomino: Application() {
             modmenu.isLocal=true
             println("la partie sera en local: ${modmenu.isLocal}")
             vue.join_game.isDisable = true
-            vue.number_id.isDisable = true
-            vue.number_key.isDisable = true
+            vue.create_game.isDisable = false
+
         }
         //radio online
         vue.online_game.setOnAction {
             modmenu.isLocal=false
             println("la partie sera en local: ${modmenu.isLocal}")
-            vue.join_game.isDisable = false
-            vue.number_id.isDisable = false
-            vue.number_key.isDisable = false
+            vue.join_game.isDisable = true
+            vue.create_game.isDisable = false
+
         }
         //nbjoueur
         vue.player_number_game.setOnAction {
             modmenu.nbjoueur=vue.player_number_game.value
             println("la partie aura ${modmenu.nbjoueur} joueurs")
+            vue.join_game.isDisable = true
+            vue.create_game.isDisable = false
         }
         //salon clé et id////////////////////////
         //formatters
@@ -60,9 +62,18 @@ class Pickomino: Application() {
         val converter = NumberStringConverter()
         vue.number_id.textFormatter = textFormatterid
         vue.number_id.textProperty().bindBidirectional(modmenu.inputid, converter)
-
+        vue.number_id.textProperty().addListener { _, _, _ ->
+            // This event will be triggered whenever the text in the TextField changes
+            vue.join_game.isDisable = false
+            vue.create_game.isDisable = true
+        }
         vue.number_key.textFormatter = textFormatterkey
         vue.number_key.textProperty().bindBidirectional(modmenu.inputkey, converter)
+        vue.number_key.textProperty().addListener { _, _, _ ->
+            // This event will be triggered whenever the text in the TextField changes
+            vue.join_game.isDisable = false
+            vue.create_game.isDisable = true
+        }
         /////////////////////////////////////////
         //bouton launch
         val contlaunch = ControleurLaunchGame(vue,modmenu,stage,modjeu)
@@ -70,8 +81,8 @@ class Pickomino: Application() {
         //bouton join
         val contjoin = ControleurJoinGame(vue,modmenu,stage,modjeu)
         vue.join_game.onAction = contjoin
-
         //default
+        vue.join_game.isDisable = true
         vue.local_game.isSelected = true
         modmenu.isLocal=true
         vue.join_game.isDisable = true
