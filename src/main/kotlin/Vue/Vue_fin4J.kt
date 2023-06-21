826ddcf1a366031ac5a6f77e3f8d6f90d42fddd7
Jules CHÉREAU <E221049W@univ-nantes.fr>
2023-06-21 15:23:43 +0200

    update VUE

--- src/main/kotlin/Vue/Vue_fin4J.kt
+++ src/main/kotlin/Vue/Vue_fin4J.kt
@@ -1,6 +1,7 @@
 package Vue
 
 import javafx.geometry.Pos
+import javafx.scene.control.Button
 import javafx.scene.control.Label
 import javafx.scene.layout.BorderPane
 import javafx.scene.layout.GridPane
@@ -11,9 +12,29 @@ class Vue_fin4J : BorderPane() {
     val title : GridPane
     val score : GridPane
 
+    val partie : BorderPane
+    val btn1 : GridPane
+    val btn2 : GridPane
+    val quitter : Button
+    val menu : Button
+
     init {
         title = GridPane()
         score  = GridPane()
+        partie  = BorderPane()
+        btn1 = GridPane()
+        btn2 = GridPane()
+
+        btn1.prefHeight = 200.0
+        btn1.prefWidth = 800.0
+
+        btn2.prefHeight = 200.0
+        btn2.prefWidth = 800.0
+
+        partie.prefHeight = 300.0
+
+        quitter = Button("Quitter")
+        menu = Button("Retourner au menu")
 
         val label = Label("Score")
 
@@ -25,6 +46,16 @@ class Vue_fin4J : BorderPane() {
 
         top = title
         center = score
+        bottom = partie
+
+        partie.left = btn1
+        partie.right = btn2
+
+        btn1.add(quitter, 0, 0)
+        btn2.add(menu, 0, 0)
+
+        btn1.alignment = Pos.CENTER
+        btn2.alignment = Pos.CENTER
 
 
         val  scoreJ1 = Label("Joueur1 : ")
@@ -37,10 +68,13 @@ class Vue_fin4J : BorderPane() {
         score.add(scoreJ3, 0, 2)
         score.add(scoreJ4, 0, 3)
 
-        scoreJ1.style = "-fx-font-weight: bold; -fx-font-size: 80px; -fx-text-fill: white;"
-        scoreJ2.style = "-fx-font-weight: bold; -fx-font-size: 80px; -fx-text-fill: white;"
-        scoreJ3.style = "-fx-font-weight: bold; -fx-font-size: 80px; -fx-text-fill: white;"
-        scoreJ4.style = "-fx-font-weight: bold; -fx-font-size: 80px; -fx-text-fill: white;"
+        scoreJ1.style = "-fx-font-weight: bold; -fx-font-size: 50px; -fx-text-fill: white;"
+        scoreJ2.style = "-fx-font-weight: bold; -fx-font-size: 50px; -fx-text-fill: white;"
+        scoreJ3.style = "-fx-font-weight: bold; -fx-font-size: 50px; -fx-text-fill: white;"
+        scoreJ4.style = "-fx-font-weight: bold; -fx-font-size: 50px; -fx-text-fill: white;"
+
+        quitter.styleClass.addAll("button", "buttonFin")
+        menu.styleClass.addAll("button", "buttonFin")
 
         score.vgap = 20.0
         score.alignment = Pos.CENTER
